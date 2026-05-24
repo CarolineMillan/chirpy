3751a3182a698efc26a3ae36cb381ee2434d9fb7
carolinemillan <carolinemillan5678@gmail.com>
2026-05-24 17:46:06 +0100

    added logo and a readiness endpoint to the server

--- main.go
+++ main.go
@@ -5,11 +5,18 @@ import (
 	"net/http"
 )
 
+func healthzHandler(res http.ResponseWriter, req *http.Request) {
+	res.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	res.WriteHeader(http.StatusOK)
+	res.Write([]byte("OK"))
+}
+
 func main() {
 
 	// create the handler for the server
 	serve_mux := http.NewServeMux()
-	serve_mux.Handle("/", http.FileServer(http.Dir(".")))
+	serve_mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	serve_mux.HandleFunc("/healthz", healthzHandler)
 
 	// create the server
 	server_struct := http.Server{}
