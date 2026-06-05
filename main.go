c6f358bebec039ef320cbd277c77a5188f00f9f6
carolinemillan <carolinemillan5678@gmail.com>
2026-06-05 15:51:19 +0100

    added handlers for request counter

--- main.go
+++ main.go
@@ -3,7 +3,42 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	// atomic int lets us safely access and update data over multiple goroutines
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	// increment the fileserverHits counter
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+
+}
+
+func (cfg *apiConfig) printHitsHandler(res http.ResponseWriter, req *http.Request) {
+	// prints the number of fileserverHits
+	num := cfg.fileserverHits.Load()
+	txt := strconv.AppendInt([]byte("Hits: "), int64(num), 10)
+
+	res.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	res.WriteHeader(http.StatusOK)
+	res.Write([]byte(txt))
+}
+
+func (cfg *apiConfig) resetHitsHandler(res http.ResponseWriter, req *http.Request) {
+	// resets the number of fileserverHits
+	txt := strconv.AppendInt([]byte("Hits: "), 0, 10)
+
+	res.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	res.WriteHeader(http.StatusOK)
+	res.Write([]byte(txt))
+}
 
 func healthzHandler(res http.ResponseWriter, req *http.Request) {
 	res.Header().Add("Content-Type", "text/plain; charset=utf-8")
@@ -14,9 +49,12 @@ func healthzHandler(res http.ResponseWriter, req *http.Request) {
 func main() {
 
 	// create the handler for the server
+	cfg := apiConfig{}
 	serve_mux := http.NewServeMux()
-	serve_mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
-	serve_mux.HandleFunc("/healthz", healthzHandler)
+	serve_mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
+	serve_mux.HandleFunc("GET /api/healthz", healthzHandler)
+	serve_mux.HandleFunc("GET /api/metrics", cfg.printHitsHandler)
+	serve_mux.HandleFunc("POST /api/reset", cfg.resetHitsHandler)
 
 	// create the server
 	server_struct := http.Server{}
